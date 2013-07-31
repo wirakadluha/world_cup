f75bde04cf2cdf695f294d7e4d11c208c36f8b87
root <root@bt.foo.org>
2013-07-31 08:15:49 +0700

    World_Cup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorldCup::Application.config.secret_token = 'f12046cfaf0e8b8ba9844c3a74eabbf8e78b2c76ca48f67745d679d7d2574d6a459378eb836be1e6ded32f09fd0354fb0ea5f39c84287e416f0c7aff8c35556a'
